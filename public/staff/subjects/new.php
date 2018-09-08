e56aebcc14da5837b01a4f3582bee2bd85e7f713
DeadBeatDesignsUK <alan36pd@gmx.co.uk>
2018-09-08 17:54:16 +0100

    Add files via upload

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -1,37 +1,70 @@
-<?php require_once('../../../private/initialize.php');
+<?php
 
-  $test = $_GET['test'] ?? '';  // PHP 7.0
+require_once('../../../private/initialize.php');
+require_login();
+if(is_post_request()) {
 
-    if ($test == '404') {
-      error404();
-    }
-      elseif ($test == '500') {
-        error500();
-      }
-        elseif ($test == 'redirect') {
-          redirect_to(url_for('/staff/subjects/index.php'));
-        }
+  $subject = [];
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
+  $subject['position'] = $_POST['position'] ?? '';
+  $subject['visible'] = $_POST['visible'] ?? '';
+
+  $result = insert_subject($subject);
+  if($result === true) {
+    $new_id = mysqli_insert_id($db);
+    $_SESSION['message'] = 'The subject was updated successfully.';
+    redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
+  } else {
+    $errors = $result;
+  }
+
+} else {
+  // display the blank form
+  $subject = [];
+  $subject["menu_name"] = '';
+  $subject["position"] = $subject_count;
+  $subject["visible"] = '';
+}
+
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set) + 1;
+mysqli_free_result($subject_set);
+
+  // $subject = [];
+  // $subject['position'] = $subject_count;
+
 ?>
+
 <?php $page_title = 'Create Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php') ?>">&laquo; Back to list</a>
+  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="subjects new">
+  <div class="subject new">
     <h1>Create Subject</h1>
 
-    <form action="<?php echo url_for('/staff/subjects/create.php'); ?>" method="post">
+    <?php echo display_errors($errors); ?>
+
+    <form action="<?php echo url_for('/staff/subjects/new.php'); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo $subject['menu_name']; ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+          <?php
+            for($i=1; $i <= $subject_count; $i++) {
+              echo "<option value=\"{$i}\"";
+              if($subject["position"] == $i) {
+                echo " selected";
+              }
+              echo ">{$i}</option>";
+            }
+          ?>
           </select>
         </dd>
       </dl>
@@ -39,12 +72,17 @@
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" />
+          <input type="checkbox" name="visible" value="1"
+          <?php if($subject['visible'] == 1) { echo " checked";} ?> />
         </dd>
       </dl>
       <div id="operations">
         <input type="submit" value="Create Subject" />
       </div>
+    </form>
+
   </div>
+
 </div>
+
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
