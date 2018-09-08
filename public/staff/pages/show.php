4bbf2b7620f71538558946e0b1c61a28b3f72b78
DeadBeatDesignsUK <alan36pd@gmx.co.uk>
2018-09-08 17:55:06 +0100

    Add files via upload

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -4,9 +4,13 @@
 
 <?php
 
-  $id = $_GET['id'] ?? '1' ; // PHP 7.0
+// admin login required
+require_login();
 
-  echo h($id);
+ $id = $_GET['id'] ?? '1' ; // PHP 7.0
+  //echo h($id);
+  $page = find_page_by_id($id);
+  $subject = find_subject_by_id($page['subject_id']);
 
 ?>
 
@@ -16,12 +20,43 @@
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to list</a>
+  <a class="back-link" href="<?php echo url_for('/staff/subjects/show.php?id='
+  . h(u($subject['id']))); ?>">&laquo; Back to Subject Page</a>
 
   <div class="page show">
 
-    Page ID <?php echo h($id); ?>
+    <h1>Page: <?php echo h($page['menu_name']); ?></h1>
 
+    <div class="actions">
+      <a class="action" href="<?php echo url_for('/index.php?id='
+      . h(u($page['id'])) . '&preview=true'); ?>" target="_blank">Preview</a>
+    </div>
+
+    <div class="attributes">
+       <dl>
+        <dt>Subject</dt>
+        <dd><?php echo h($subject['menu_name']); ?></dd>
+      </dl>
+       <dl>
+        <dt>Menu Name</dt>
+        <dd><?php echo h($page['menu_name']); ?></dd>
+      </dl>
+      <dl>
+       <dt>Position</dt>
+       <dd><?php echo h($page['position']); ?></dd>
+     </dl>
+     <dl>
+      <dt>Visible</dt>
+      <dd><?php echo $page['visible'] == '1' ? 'true' : 'false'; ?></dd>
+    </dl>
+     <dl>
+      <dt>Content</dt>
+      <dd><?php echo h($page['content']); ?></dd>
+    </dl>
+  </div>
+
+
+</div>
   </div>
 
 </div>
